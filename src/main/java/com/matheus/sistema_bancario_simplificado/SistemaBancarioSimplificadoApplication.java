88cd91a0c5219407834f287bcd6c5ca705b79603
Matheus José <mathjjc72@gmail.com>
2026-09-23 23:09:10 -0300

    inicialização do projeto sistema bancario simplificado

---
+++ src/main/java/com/matheus/sistema_bancario_simplificado/SistemaBancarioSimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.sistema_bancario_simplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBancarioSimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBancarioSimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/sistema_bancario_simplificado/SistemaBancarioSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.sistema_bancario_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancarioSimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
